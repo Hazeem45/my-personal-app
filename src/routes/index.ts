98c73efc1fde0ad73a35bed0634f0f1ba6da1ce9
Hazeem <hasyim.fery27@gmail.com>
2025-07-26 09:41:51 +0700

    add interface

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,15 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", { title: "Home" });
+});
+
+router.get("/contact", (req, res) => {
+  res.render("contact", { title: "Contact", formStyle: "form" });
+});
+
+router.get("/project", (req, res) => {
+  res.render("project", { title: "My Project", formStyle: "form" });
 });
 
 export default router;
